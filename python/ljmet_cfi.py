4e977a11a143243c746f8b409345ac77c99973b8
Jesus Orduna <orduna.jesus@gmail.com>
2014-12-01 17:07:18 -0600

    Including Dylan's modifications and clean up

--- python/ljmet_cfi.py
+++ python/ljmet_cfi.py
@@ -1,9 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 ljmet = cms.PSet(
-    isMc      = cms.bool(True),
-    verbosity = cms.int32(0),
-    useHcalLaserEventFilter = cms.bool(True),
-    runs                 = cms.vint32([]),
-    excluded_calculators = cms.vstring()
-    )
+                 isMc      = cms.bool(True),
+                 verbosity = cms.int32(0),
+                 runs                 = cms.vint32([]),
+                 excluded_calculators = cms.vstring()
+                 )
