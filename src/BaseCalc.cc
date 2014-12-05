df3a0cd8204ca92d02af87389cbea61aed704c86
Jesus Orduna <orduna.jesus@gmail.com>
2014-12-05 14:17:43 -0600

    Add implementation for SetValue for std::vector<bool>

--- src/BaseCalc.cc
+++ src/BaseCalc.cc
@@ -1,130 +1,121 @@
 /*
-  Base class for all calculators
-
-   Author: Gena Kukartsev, 2012
-*/
-
+ Base class for all calculators
+ 
+ Author: Gena Kukartsev, 2012
+ */
 
 
 #include "LJMet/Com/interface/BaseCalc.h"
 #include "LJMet/Com/interface/LjmetEventContent.h"
 
-
-
 BaseCalc::BaseCalc():
-  mName(""),
-  mLegend(""){
+mName(""),
+mLegend("")
+{
 }
 
-
-
-BaseCalc::~BaseCalc(){
-
+BaseCalc::~BaseCalc()
+{
 }
 
-
-
-void BaseCalc::init(){
-  //
-  // private init method to be called by LjmetFactory
-  // when registering the calculator
-  //
-
-  mLegend = "["+mName+"]: ";
-  std::cout << mLegend << "registering "
-	    << mName << std::endl;
-
-  return;
+void BaseCalc::init()
+{
+    //
+    // private init method to be called by LjmetFactory
+    // when registering the calculator
+    //
+    
+    mLegend = "["+mName+"]: ";
+    std::cout << mLegend << "registering " << mName << std::endl;
+    
+    return;
 }
 
-
-
-std::string BaseCalc::GetName(){
-  return mName;
+std::string BaseCalc::GetName()
+{
+    return mName;
 }
 
-
-
-int BaseCalc::ProduceEvent(edm::EventBase const & event, BaseEventSelector * selector){
-  return 0;
+int BaseCalc::ProduceEvent(edm::EventBase const & event, BaseEventSelector * selector)
+{
+    return 0;
 }
 
-
-
-int BaseCalc::AnalyzeEvent(edm::EventBase const & event, BaseEventSelector * selector){
-  return 0;
+int BaseCalc::AnalyzeEvent(edm::EventBase const & event, BaseEventSelector * selector)
+{
+    return 0;
 }
 
-
-
-int BaseCalc::EndJob(){
-  return 0;
+int BaseCalc::EndJob()
+{
+    return 0;
 }
 
-
-
-void BaseCalc::setName(std::string name){
-  mName = name;
+void BaseCalc::setName(std::string name)
+{
+    mName = name;
 }
 
-
-
-void BaseCalc::SetValue(std::string name, int value){
-  std::string _name = name+"_"+mName;
-  mpEc->SetValue(_name, value);
+void BaseCalc::SetValue(std::string name, bool value)
+{
+    std::string _name = name+"_"+mName;
+    mpEc->SetValue(_name, value);
 }
 
-
-
-void BaseCalc::SetValue(std::string name, double value){
-  std::string _name = name+"_"+mName;
-  mpEc->SetValue(_name, value);
+void BaseCalc::SetValue(std::string name, int value)
+{
+    std::string _name = name+"_"+mName;
+    mpEc->SetValue(_name, value);
 }
 
-void BaseCalc::SetValue(std::string name, std::vector<int> value){
-  std::string _name = name+"_"+mName;
-  mpEc->SetValue(_name, value);
+void BaseCalc::SetValue(std::string name, double value)
+{
+    std::string _name = name+"_"+mName;
+    mpEc->SetValue(_name, value);
 }
 
-void BaseCalc::SetValue(std::string name, std::vector<double> value){
-  std::string _name = name+"_"+mName;
-  mpEc->SetValue(_name, value);
+void BaseCalc::SetValue(std::string name, std::vector<bool> value)
+{
+    std::string _name = name+"_"+mName;
+    mpEc->SetValue(_name, value);
 }
 
-
-
-void BaseCalc::SetEventContent(LjmetEventContent * pEc){
-  mpEc = pEc;
-  return;
+void BaseCalc::SetValue(std::string name, std::vector<int> value)
+{
+    std::string _name = name+"_"+mName;
+    mpEc->SetValue(_name, value);
 }
 
-
-
-void BaseCalc::SetPSet(edm::ParameterSet pset){
-  mPset = pset;
-  return;
+void BaseCalc::SetValue(std::string name, std::vector<double> value)
+{
+    std::string _name = name+"_"+mName;
+    mpEc->SetValue(_name, value);
 }
 
-
-
-void BaseCalc::SetHistogram(std::string name,
-				     int nbins, 
-				     double low, 
-				     double high){
-  //
-  // Declare a new histogram to be created for the module
-  //
-
-  mpEc->SetHistogram(mName, name, nbins, low, high);
-
-  return;
+void BaseCalc::SetEventContent(LjmetEventContent * pEc)
+{
+    mpEc = pEc;
+    return;
 }
 
+void BaseCalc::SetPSet(edm::ParameterSet pset)
+{
+    mPset = pset;
+    return;
+}
 
+void BaseCalc::SetHistogram(std::string name, int nbins, double low, double high)
+{
+    //
+    // Declare a new histogram to be created for the module
+    //
+    
+    mpEc->SetHistogram(mName, name, nbins, low, high);
+    return;
+}
 
-void BaseCalc::SetHistValue(std::string name, 
-				     double value){
-  mpEc->SetHistValue(mName, name, value);
-
-  return;
+void BaseCalc::SetHistValue(std::string name, double value)
+{
+    mpEc->SetHistValue(mName, name, value);
+    return;
 }
