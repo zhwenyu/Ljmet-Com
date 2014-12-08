226f7b35508379eb9b8fbce36dcea47fc23a9a7b
Jesus Orduna <orduna.jesus@gmail.com>
2014-12-08 11:34:13 -0600

    Extra comma at the end

--- python/CATopoCalc_cfi.py
+++ python/CATopoCalc_cfi.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 JetSubCalc = cms.PSet(
                       debug             = cms.bool(False),
-                      AK8slimmedJetColl = cms.InputTag("slimmedJetsAK8"),
+                      AK8slimmedJetColl = cms.InputTag("slimmedJetsAK8")
                       )
