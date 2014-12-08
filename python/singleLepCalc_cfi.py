314e6b9e5c3c1b726b8884a324b9e6e62023cca3
Jesus Orduna <orduna.jesus@gmail.com>
2014-12-08 10:53:36 -0600

    Update to configuration file

--- python/singleLepCalc_cfi.py
+++ python/singleLepCalc_cfi.py
@@ -1,8 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 singleLepCalc = cms.PSet(
-    triggerSummary = cms.InputTag("hltTriggerSummaryAOD"),
-    rhoSrc     = cms.InputTag("kt6PFJets", 'rho'),
-    isWJets     = cms.bool(False),
-    isTB       = cms.bool(False)
-)
+                         triggerSummary    = cms.InputTag("hltTriggerSummaryAOD"),
+                         triggerCollection = cms.InputTag("TriggerResults::HLT"),
+                         rhoSrc            = cms.InputTag("fixedGridRhoAll"),
+#                         muonSrc           = cms.InputTag("slimmedMuons"),
+                         isWJets           = cms.bool(False),
+                         isTB              = cms.bool(False)
+                         )
