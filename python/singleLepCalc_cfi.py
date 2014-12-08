62aa45984980877e970c90f9ed2edf369265f1f9
Jesus Orduna <orduna.jesus@gmail.com>
2014-12-08 11:11:27 -0600

    Update to configuration file

--- python/singleLepCalc_cfi.py
+++ python/singleLepCalc_cfi.py
@@ -4,7 +4,6 @@ singleLepCalc = cms.PSet(
                          triggerSummary    = cms.InputTag("hltTriggerSummaryAOD"),
                          triggerCollection = cms.InputTag("TriggerResults::HLT"),
                          rhoSrc            = cms.InputTag("fixedGridRhoAll"),
-#                         muonSrc           = cms.InputTag("slimmedMuons"),
                          isWJets           = cms.bool(False),
                          isTB              = cms.bool(False)
                          )
