97023d5491b7fdebc9463e88e3e5ab51d4c5fe82
Jesus Orduna <orduna.jesus@gmail.com>
2014-12-08 11:33:16 -0600

    Missing comma

--- python/CATopoCalc_cfi.py
+++ python/CATopoCalc_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 JetSubCalc = cms.PSet(
-                      debug             = cms.bool(False)
+                      debug             = cms.bool(False),
                       AK8slimmedJetColl = cms.InputTag("slimmedJetsAK8"),
                       )
