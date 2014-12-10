0147f5301408ec8cebbc2e013a172321586901ab
Jesus Orduna <orduna.jesus@gmail.com>
2014-12-09 23:50:24 -0600

    Fixed tagInfo

--- python/JetSubCalc_cfi.py
+++ python/JetSubCalc_cfi.py
@@ -1,11 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 # available bDiscrimiant:
+# - combinedInclusiveSecondaryVertexV2BJetTags
+# - combinedMVABJetTags
 # - jetBProbabilityBJetTags
 # - jetProbabilityBJetTags
+# - pfCombinedSecondaryVertexBJetTags
+# - simpleSecondaryVertexHighEffBJetTags
 # - trackCountingHighPurBJetTags
 # - trackCountingHighEffBJetTags
-# - simpleSecondaryVertexHighEffBJetTags
 # - simpleSecondaryVertexHighPurBJetTags
 # - combinedSecondaryVertexBJetTags (this is the default)
 # - combinedInclusiveSecondaryVertexBJetTags
@@ -13,6 +16,7 @@ import FWCore.ParameterSet.Config as cms
 JetSubCalc = cms.PSet(
                       slimmedJetColl     = cms.InputTag("slimmedJets"),
                       slimmedJetsAK8Coll = cms.InputTag("slimmedJetsAK8"),
+                      caTopTagInfosColl  = cms.string("caTopTagInfos"),
                       bDiscriminant      = cms.string("combinedSecondaryVertexBJetTags"),
-                      tagInfo            = cms.string("CATop")
+                      tagInfo            = cms.string("caTop")
                       )
