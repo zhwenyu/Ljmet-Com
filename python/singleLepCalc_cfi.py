55e8fb85435532b6ffd26e2aac4cb2d3e7fc8a9d
Dylan <drankin@bu.edu>
2015-02-08 18:16:21 -0600

    adding trigger matching parameters

--- python/singleLepCalc_cfi.py
+++ python/singleLepCalc_cfi.py
@@ -1,9 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 singleLepCalc = cms.PSet(
-                         triggerSummary    = cms.InputTag("hltTriggerSummaryAOD"),
-                         triggerCollection = cms.InputTag("TriggerResults::HLT"),
+                         dataType          = cms.string('All'),
+                         isMc              = cms.bool(True),
+                         pvCollection = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                         genParticles = cms.InputTag("prunedGenParticles"),
+			 triggerCollection = cms.InputTag("TriggerResults::HLT"),
+			 triggerSummary = cms.InputTag("selectedPatTrigger"),
+                         keepFullMChistory = cms.bool(True),
+                         keepPDGID    = cms.vuint32(1, 2, 3, 4, 5, 21, 11, 12, 13, 14, 15, 16),
+                         keepMomPDGID = cms.vuint32(6, 24),
                          rhoSrc            = cms.InputTag("fixedGridRhoAll"),
                          isWJets           = cms.bool(False),
-                         isTB              = cms.bool(False)
+                         isTB              = cms.bool(False),
                          )
