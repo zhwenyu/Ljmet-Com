3d2f6e776ed13f86000d6167005e7127a1bf3562
drankincms <drankin@bu.edu>
2015-09-14 13:54:54 +0200

    Update singleLepEventSelector.cc

--- src/singleLepEventSelector.cc
+++ src/singleLepEventSelector.cc
@@ -3,7 +3,7 @@
 // FWLite PAT analyzer-selector for a generic single lepton analysis
 //
 // Adapted from WprimeEventSelector
-// Joshua Swanson, April 2014
+// Joshua Swanson, April 2014 TEST
 //
 #ifndef LJMet_Com_interface_singleLepEventSelector_h
 #define LJMet_Com_interface_singleLepEventSelector_h
