9e235740586659440a02be1c48b0f8e7b1dc7e8f
Dylan <drankin@bu.edu>
2015-09-21 03:44:20 -0500

    adding Summer15 50ns V5 JECs, cmsRun config to dump JECs from sqlite db file, and 50ns golden json file

---
+++ data/dumpJEC_sqlite_cfg.py
@@ -0,0 +1,45 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("jectxt")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+# define your favorite global tag
+process.GlobalTag.globaltag = '74X_dataRun2_Prompt_v1'
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+from CondCore.DBCommon.CondDBSetup_cfi import *
+process.jec = cms.ESSource("PoolDBESSource",
+      DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0)
+        ),
+      timetype = cms.string('runnumber'),
+      toGet = cms.VPSet(
+      cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            tag    = cms.string('JetCorrectorParametersCollection_Summer15_50nsV5_DATA_AK4PFchs'),
+            label  = cms.untracked.string('AK4PFchs')
+            ),
+      cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+            tag    = cms.string('JetCorrectorParametersCollection_Summer15_50nsV5_DATA_AK8PFchs'),
+            label  = cms.untracked.string('AK8PFchs')
+            ),
+      ## here you add as many jet types as you need
+      ## note that the tag name is specific for the particular sqlite file 
+      ), 
+      connect = cms.string('sqlite:Summer15_50nsV5_DATA.db')
+)
+## add an es_prefer statement to resolve a possible conflict from simultaneous connection to a global tag
+process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
+process.source = cms.Source("EmptySource")
+process.readAK4PF    = cms.EDAnalyzer('JetCorrectorDBReader',  
+      # below is the communication to the database 
+      payloadName    = cms.untracked.string('AK4PFchs'),
+      # this is used ONLY for the name of the printed txt files. You can use any name that you like, 
+      # but it is recommended to use the GT name that you retrieved the files from.
+      globalTag      = cms.untracked.string('Summer15_50nsV5_DATA'),
+      printScreen    = cms.untracked.bool(False),
+      createTextFile = cms.untracked.bool(True)
+)
+process.readAK8PF = process.readAK4PF.clone(payloadName = 'AK8PFchs')
+process.p = cms.Path(process.readAK4PF * process.readAK8PF)
