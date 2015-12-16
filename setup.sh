f31cab731ab93932412f447cbddf30fe7a316075
Dylan <drankin@bu.edu>
2015-12-16 11:18:06 -0600

    fixing order

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-setenv LHAPDF_DATA_PATH ${LHAPDF_DATA_PATH}:/afs/cern.ch/sw/lcg/external/lhapdfsets/current/:${CMSSW_BASE}/src/LJMet/Com/data/PDFsets
+setenv LHAPDF_DATA_PATH ${LHAPDF_DATA_PATH}:${CMSSW_BASE}/src/LJMet/Com/data/PDFsets:/afs/cern.ch/sw/lcg/external/lhapdfsets/current/
 echo $LHAPDF_DATA_PATH
