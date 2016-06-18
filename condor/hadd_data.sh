9f35103b2f352ddcd70131dc0461cbca5f4e3e41
Clint <clint.a.richardson@gmail.com>
2016-06-18 04:59:07 -0500

    adding scripts to hadd

---
+++ condor/hadd_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+hadd /eos/uscms/store/user/clint/May25/ljmet_trees/ljmet_Data_ElEl.root /eos/uscms/store/user/clint/Spring15/25ns/May25/DoubleEG*/*.root
+hadd /eos/uscms/store/user/clint/May25/ljmet_trees/ljmet_Data_ElMu.root /eos/uscms/store/user/clint/Spring15/25ns/May25/MuonEG*/*.root
+hadd /eos/uscms/store/user/clint/May25/ljmet_trees/ljmet_Data_MuMu.root /eos/uscms/store/user/clint/Spring15/25ns/May25/DoubleMu*/*.root
---
+++ condor/hadd_fr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+hadd /eos/uscms/store/user/clint/FakeRate/ljmet_trees/ljmet_FakeRate_El.root /eos/uscms/store/user/clint/FakeRate/25ns/May25/DoubleEG*/*.root
+#hadd /eos/uscms/store/user/clint/FakeRate/ljmet_trees/ljmet_FakeRate_Mu.root /eos/uscms/store/user/clint/FakeRate/25ns/May25/DoubleMu*/*.root
