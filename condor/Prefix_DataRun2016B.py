cd11c74af06e2c4fad255cd5018a49bd597d7043
Clint <clint.a.richardson@gmail.com>
2016-07-19 10:54:18 -0500

    updatd to 2016 latest

--- condor/Prefix_DataRun2016B.py
+++ condor/Prefix_DataRun2016B.py
@@ -1,6 +1,6 @@
-DoubleEG-PromptReco-v1
-DoubleMuon-PromptReco-v1
-MuonEG-PromptReco-v1
-DoubleEG-PromptReco-v2
-DoubleMuon-PromptReco-v2
-MuonEG-PromptReco-v2
+DoubleEG-Run2016B-PromptReco-v2
+DoubleMuon-Run2016B-PromptReco-v2
+MuonEG-Run2016B-PromptReco-v2
+DoubleEG-Run2016C-PromptReco-v2
+DoubleMuon-Run2016C-PromptReco-v2
+MuonEG-Run2016C-PromptReco-v2
--- condor/hadd_bkg.sh
+++ condor/hadd_bkg.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #tt+X
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_TTW.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/TTW/*.root
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_TTZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/TTZ/*.root
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_TTH.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/TTH/*.root
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_TTTT.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/TTTT/*.root
+#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_TTW.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/TTW/*.root
+#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_TTZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/TTZ/*.root
+#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_TTH.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/TTH/*.root
+#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_TTTT.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/TTTT/*.root
 #diboson
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_WZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/WZ/*.root
-hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_ZZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ZZ/*.root
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_WpWp.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/WpWp/*.root
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_VH.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/VH/*.root
+hadd -f /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_WZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/WZ/*.root
+hadd -f /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_ZZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ZZ/*.root
+#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_WpWp.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/WpWp/*.root
+#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_VH.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/VH/*.root
 #triboson
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_WWW.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/WWW/*.root
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_WWZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/WWZ/*.root
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_WZZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/WZZ/*.root
-#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ljmet_trees/ljmet_ZZZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/June18/ZZZ/*.root
+#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_WWW.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/WWW/*.root
+hadd -f /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_WWZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/WWZ/*.root
+#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_WZZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/WZZ/*.root
+#hadd /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ljmet_trees/ljmet_ZZZ.root /eos/uscms/store/user/lpctlbsm/clint/Spring16/25ns/July12/ZZZ/*.root
 
