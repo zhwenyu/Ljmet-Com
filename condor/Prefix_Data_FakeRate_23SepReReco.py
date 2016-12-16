95380e84ed862a0de4d0662339c6554a8dd2e59b
Clint <clint.a.richardson@gmail.com>
2016-12-16 14:36:14 -0600

    adding files

---
+++ condor/Prefix_Data_FakeRate_23SepReReco.py
@@ -0,0 +1,16 @@
+DoubleEG-Run2016B-23Sep2016-v3
+DoubleEG-Run2016C-23Sep2016-v1
+DoubleEG-Run2016D-23Sep2016-v1
+DoubleEG-Run2016E-23Sep2016-v1
+DoubleEG-Run2016F-23Sep2016-v1
+DoubleEG-Run2016G-23Sep2016-v1
+DoubleEG-Run2016H-PromptReco-v2
+DoubleEG-Run2016H-PromptReco-v3
+DoubleMuon-Run2016B-23Sep2016-v3
+DoubleMuon-Run2016C-23Sep2016-v1
+DoubleMuon-Run2016D-23Sep2016-v1
+DoubleMuon-Run2016E-23Sep2016-v1
+DoubleMuon-Run2016F-23Sep2016-v1
+DoubleMuon-Run2016G-23Sep2016-v1
+DoubleMuon-Run2016H-PromptReco-v2
+DoubleMuon-Run2016H-PromptReco-v3
