fbbd8e76e5e45c343e69abc98d4826dd7e486acc
jmhogan <jmhogan@fnal.gov>
2017-01-28 20:38:58 -0600

    edits

--- condor_TTinclusive/submitLJMet.sh
+++ condor_TTinclusive/submitLJMet.sh
@@ -6,27 +6,27 @@ cp TTsinglelep_Data_cfg.py ljmet_cfg.py
 
 python -u condor_submitData2016.py nominal #>& submit_Data.log &
 
-echo "SUBMITTING LJMET -- nominal"
+# echo "SUBMITTING LJMET -- nominal"
 
-cp TTsinglelep_MC_cfg.py ljmet_cfg.py
+# cp TTsinglelep_MC_cfg.py ljmet_cfg.py
 
-python -u condor_submitSpring2016.py nominal #>& submit_MC_nominal.log &
+# python -u condor_submitMoriond2017.py nominal #>& submit_MC_nominal.log &
 
-echo "SUBMITTING LJMET -- JECup"
+# echo "SUBMITTING LJMET -- JECup"
 
-python -u condor_submitSpring2016.py JECup #>& submit_MC_JECup.log &
+# python -u condor_submitMoriond2017.py JECup #>& submit_MC_JECup.log &
 
-echo "SUBMITTING LJMET -- JECdown"
+# echo "SUBMITTING LJMET -- JECdown"
 
-python -u condor_submitSpring2016.py JECdown #>& submit_MC_JECdown.log &
+# python -u condor_submitMoriond2017.py JECdown #>& submit_MC_JECdown.log &
 
-echo "SUBMITTING LJMET -- JERup"
+# echo "SUBMITTING LJMET -- JERup"
 
-python -u condor_submitSpring2016.py JERup #>& submit_MC_JERup.log &
+# python -u condor_submitMoriond2017.py JERup #>& submit_MC_JERup.log &
 
-echo "SUBMITTING LJMET -- JERdown"
+# echo "SUBMITTING LJMET -- JERdown"
 
-python -u condor_submitSpring2016.py JERdown #>& submit_MC_JERdown.log &
+# python -u condor_submitMoriond2017.py JERdown #>& submit_MC_JERdown.log &
 
 ############ DON'T NEED ANYMORE FOR MORIOND17
 # echo "SUBMITTING LJMET -- nominal"
